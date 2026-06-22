9dc369fe114c01d00cd465b13cbc11c71286b3f1
chairtablecomputer <chairtablecomputer@gmail.com>
2026-06-22 13:55:50 +0800

    Update main.tf
    Change SG cidr to public IP: 203.149.196.173

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -3472,7 +3472,7 @@ resource "aws_security_group" "goat_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["203.149.196.173/32"]
   }
   egress {
     from_port   = 0
